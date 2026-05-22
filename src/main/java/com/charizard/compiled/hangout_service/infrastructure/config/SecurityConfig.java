ca3e091872e80d9c4c1ebeadd17e2c1e6e39c381
Cristian Guerrero <cristian.guerrero-b@mail.escuelaing.edu.co>
2026-05-22 11:46:56 -0500

    Feat: Correccion Bugs 2

--- src/main/java/com/charizard/compiled/hangout_service/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/charizard/compiled/hangout_service/infrastructure/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -38,7 +39,7 @@ public class SecurityConfig {
                     "/actuator/health/**"
                 ).permitAll()
                 .requestMatchers(org.springframework.http.HttpMethod.GET, "/api/v1/parches").permitAll()
-                .requestMatchers(org.springframework.http.HttpMethod.GET, "/api/v1/parches/**/check").permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/v1/parches/*/miembros/*/check", "GET")).permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(kongAuthFilter, UsernamePasswordAuthenticationFilter.class);
